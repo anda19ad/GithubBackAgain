21bbe8082641dd962b013d34962608aacee495dc
anda19ad <anda19ad@student.cbs.dk>
2021-09-08 10:47:13 +0200

    Endnu en linje

--- App.js
+++ App.js
@@ -5,7 +5,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>En mobil app. Der nu er klar til brug. Hvad med nu?</Text>
+      <Text>Endnu en linje til min AAp</Text>
       <StatusBar style="auto" />
     </View>
   );
